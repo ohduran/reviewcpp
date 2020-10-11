717b40ce8e7b32767301006ffac24ddd500632b9
ohduran <alvaro.duranb@hotmail.com>
2020-10-11 19:01:24 +0200

    chapter-4

---
+++ chapter-4/void.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void writeValue(int x)
+{
+
+    std::cout << "The value of x is " << x << "\n";
+
+    //no return statement
+}
+
+int main()
+{
+    writeValue(4);
+    return 0;
+}
