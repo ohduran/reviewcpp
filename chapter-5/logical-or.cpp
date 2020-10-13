02222fe8188f00dd8e8944d23352f6a82ec08df5
Alvaro <alvaro.duranb@hotmail.com>
2020-10-13 06:34:29 +0200

    end chapter 5

---
+++ chapter-5/logical-or.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter a number: ";
+    int value;
+    std::cin >> value;
+
+    if (value == 0 || value == 1)
+    {
+        std::cout << "You picked 0 or 1\n";
+    }
+    else
+    {
+        std::cout << "You did not picked 0 or 1\n";
+    }
+
+    return 0;
+}
